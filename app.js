75beeafed8eebc680e9dd496b69707ed8643be1f
melon <meloniusz@hotmail.com>
2020-02-26 20:53:10 +0800

    changes

--- app.js
+++ app.js
@@ -14,17 +14,13 @@ const userRoutes = require("./routes/user");
 
 const port = process.env.PORT || 8000;
 
-//db connection
+//database connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true
   })
   .then(() => console.log("DB Connected"));
-
-mongoose.connection.on("error", err => {
-  console.log(`DB connection error: ${err.message}`);
-});
 
 //middlewares
 app.use(morgan("dev"));
