c9e6714cd229849bd49a438934d7174c92d22437
melon <meloniusz@hotmail.com>
2020-02-27 17:03:47 +0800

    small corrections to validator

--- validator/index.js
+++ validator/index.js
@@ -1,9 +1,9 @@
-exports.userSignupValidator = (req, res) => {
-  req.check("name", "name is required").notEmpty();
+exports.userSignupValidator = (req, res, next) => {
+  req.check("name", "Name is required").notEmpty();
   req
     .check("email", "Email must be between 3 to 32 characters")
-    .matches(/.+@.+\..+/)
-    .withMEssage("Email must contain @")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
     .isLength({
       min: 4,
       max: 32
@@ -12,10 +12,12 @@ exports.userSignupValidator = (req, res) => {
   req
     .check("password")
     .isLength({ min: 6 })
-    .withMessage("Password must contain at least 6 characters");
+    .withMessage("Password must contain at least 6 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number");
   const errors = req.validationErrors();
   if (errors) {
-    const firstError = errors.map(error.msg)[0];
+    const firstError = errors.map(error => error.msg)[0];
     return res.status(400).json({ error: firstError });
   }
   next();
