f155470a2ecc121ec6bc5eb52c56579380663d2a
melon <meloniusz@hotmail.com>
2020-02-27 17:16:40 +0800

    creating user controller for signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
-
 exports.signup = (req, res) => {
   //   console.log("req.body", req.body);
   const user = new User(req.body);
@@ -16,4 +17,17 @@ exports.signup = (req, res) => {
       user
     });
   });
+};
+
+exports.signin = (req, res) => {
+  //find the user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: "User with that email does not exist. Please signup"
+      });
+    }
+  });
+  //if user is found make sure the email and password match
 };
