97eb7e09ec98b460c2ecc41c6f7a35962d88a7f2
melon <meloniusz@hotmail.com>
2020-02-27 16:53:28 +0800

    password validation done

--- validator/index.js
+++ validator/index.js
@@ -2,9 +2,15 @@ exports.userSignupValidator = (req, res) => {
   req.check("name", "name is required").notEmpty();
   req
     .check("email", "Email must be between 3 to 32 characters")
-    .matches(/.+@.+\..+/).withMEssage("Email must contain @").isLength({
-        min: 4,
-        max: 32
+    .matches(/.+@.+\..+/)
+    .withMEssage("Email must contain @")
+    .isLength({
+      min: 4,
+      max: 32
     });
-    req.check("password", "Password is required").notEmpty()
+  req.check("password", "Password is required").notEmpty();
+  req
+    .check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must contain at least 6 characters");
 };
