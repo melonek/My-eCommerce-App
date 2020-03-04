6ea1e19db18dcc5cca38cd68f78e76696505066a
melon <meloniusz@hotmail.com>
2020-03-04 23:23:15 +0800

    small correction to the read CRUD method

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.get("/product/productId", read);
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
