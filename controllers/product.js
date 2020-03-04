c9b05aa5b98250b6ae7fc8454185e369a8401788
melon <meloniusz@hotmail.com>
2020-03-04 22:48:29 +0800

    small correction to max file size liit

--- controllers/product.js
+++ controllers/product.js
@@ -12,11 +12,28 @@ exports.create = (req, res) => {
         error: "Image could not be uploaded"
       });
     }
+
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required"
+      });
+    }
+
     let product = new Product(fields);
 
     if (files.photo) {
       console.log("FILES PHOTO: ", files.photo);
-      if (files.photo.size > 100000) {
+      if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: "Image should be less than 1mb in size"
         });
