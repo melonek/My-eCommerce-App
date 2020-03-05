a1d2b20b874233f9ddbcfbcb8661cb5fb04c4556
melon <meloniusz@hotmail.com>
2020-03-05 19:57:54 +0800

    working on category by id = crated route and middleware in controllers

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,18 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.categoryByID = (req, res, next) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
