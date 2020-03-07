bcd6f0ff1bfc03250bdd28493dfd4345146ddc4b
melon <meloniusz@hotmail.com>
2020-03-07 14:07:48 +0800

    writing the product controller method for related products

--- controllers/product.js
+++ controllers/product.js
@@ -172,5 +172,20 @@ exports.list = (req, res) => {
  */
 
 exports.listRelated = (req, res) => {
-    let limit = 
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+  Product.find({
+    _id: { $ne: req.product },
+    category: req.product.category
+  })
+    .limit(limit)
+    .populate("category", "_id name")
+    .exec((err, products) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Products not found"
+        });
+      }
+      res.json(products);
+    });
 };
