a7bdae6716386ef70b036fda356f9fc6f00ca650
melon <meloniusz@hotmail.com>
2020-03-07 15:21:08 +0800

    added npm cors to the package

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 
 //import routes
