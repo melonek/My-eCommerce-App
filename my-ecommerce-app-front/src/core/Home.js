e8e64d92060c0b915fdde6156a663e7c7ae7fd3d
melon <meloniusz@hotmail.com>
2020-03-10 19:19:46 +0800

    working on Layout

--- my-ecommerce-app-front/src/core/Home.js
+++ my-ecommerce-app-front/src/core/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
-
-const Home = () => <div>Home</div>;
+import Layout from "./Layout";
+const Home = () => (
+  <Layout title="Home Page" description="Node React e-Commerce App">
+    ...
+  </Layout>
+);
 
 export default Home;
