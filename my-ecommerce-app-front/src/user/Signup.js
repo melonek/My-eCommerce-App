a13ed1b41f13863ffac6bc8f9f194c9fc32a1622
melon <meloniusz@hotmail.com>
2020-03-10 20:20:44 +0800

    created a config file

--- my-ecommerce-app-front/src/user/Signup.js
+++ my-ecommerce-app-front/src/user/Signup.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signup = () => <div>Signup</div>;
+const Signup = () => (
+  <Layout title="Signup" description="Signup to Node React e-Commerce App">
+    {process.env.REACT_APP_API_URL}
+  </Layout>
+);
 
 export default Signup;
