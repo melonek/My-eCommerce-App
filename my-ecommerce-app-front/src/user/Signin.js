f88b4b9e931fb73d0a41b46c68fdf5ef090b1cd2
melon <meloniusz@hotmail.com>
2020-03-11 17:26:28 +0800

    user signin working - redirecting to homepage when successfull

--- my-ecommerce-app-front/src/user/Signin.js
+++ my-ecommerce-app-front/src/user/Signin.js
@@ -1,10 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
+import { API } from "../config";
+import { signin } from "../auth";
 
-const Signin = () => (
-  <Layout title="Signin" description="Signin to Node React e-Commerce App">
-    ...
-  </Layout>
-);
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToRefferer: false
+  });
+
+  const { email, password, loading, error, redirectToRefferer } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          redirectToRefferer: true
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-info">
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToRefferer) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  return (
+    <Layout
+      title="Signup"
+      description="Signup to Node React e-Commerce App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showLoading()}
+      {showError()}
+      {signUpForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 
 export default Signin;
