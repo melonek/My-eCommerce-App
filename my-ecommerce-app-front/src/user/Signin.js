bd3dc348db1b7c62337faf92810cb6a3630f9e1f
melon <meloniusz@hotmail.com>
2020-03-11 19:28:59 +0800

    finished the user dashboard

--- my-ecommerce-app-front/src/user/Signin.js
+++ my-ecommerce-app-front/src/user/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
 import { API } from "../config";
-import { signin, authenticate } from "../auth";
+import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -14,6 +14,7 @@ const Signin = () => {
   });
 
   const { email, password, loading, error, redirectToRefferer } = values;
+  const { user } = isAuthenticated();
 
   const handleChange = name => event => {
     setValues({ ...values, error: false, [name]: event.target.value });
@@ -80,7 +81,11 @@ const Signin = () => {
 
   const redirectUser = () => {
     if (redirectToRefferer) {
-      return <Redirect to="/" />;
+      if (user && user.role === 1) {
+        return <Redirect to="/admin/dashboard" />;
+      } else {
+        return <Redirect to="/user/dashboard" />;
+      }
     }
   };
 
