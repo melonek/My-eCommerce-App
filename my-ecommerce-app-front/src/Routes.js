dbf910e77bb4003b2e1629e6138f611b37c0e529
melon <meloniusz@hotmail.com>
2020-03-11 18:23:19 +0800

    working on private route for authenticated users only

--- my-ecommerce-app-front/src/Routes.js
+++ my-ecommerce-app-front/src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
+import PrivateRoute from "./auth/PrivateRoute";
+import Dashboard from "./user/UserDashboard";
 
 const Routes = () => {
   return (
---
+++ my-ecommerce-app-front/src/user/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from "React";
+import Layout from "../core/Layout";
+
+const Dashboard = () => {
+  return (
+    <Layout title="Dashboard" description="User Dashboard">
+      ...
+    </Layout>
+  );
+};
+
+export default Dashboard;
