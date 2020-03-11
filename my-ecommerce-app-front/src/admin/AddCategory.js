a02b0a5554eb589013b1e97370edd7bb0d23f9c0
melon <meloniusz@hotmail.com>
2020-03-11 21:57:40 +0800

    working on the category success and error when adding

--- my-ecommerce-app-front/src/admin/AddCategory.js
+++ my-ecommerce-app-front/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState("");
@@ -21,6 +22,14 @@ const AddCategory = () => {
     setError("");
     setSuccess(false);
     //make request to api to create category
+    createCategory(user._id, token, { name }).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setError("");
+        setSuccess(true);
+      }
+    });
   };
 
   const newCategoryForm = () => (
@@ -39,6 +48,18 @@ const AddCategory = () => {
     </form>
   );
 
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className="text=danger">{name} is created</h3>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <h3 className="text=danger">{name} should be unique</h3>;
+    }
+  };
+
   return (
     <Layout
       title="Add a new category"
@@ -46,7 +67,11 @@ const AddCategory = () => {
       className="container"
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {showSuccess()}
+          {showError()}
+          {newCategoryForm()}
+        </div>
       </div>
     </Layout>
   );
