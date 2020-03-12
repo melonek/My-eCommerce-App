5f6012fe67db0d8d9c1eef7cc38614602ac53fa6
melon <meloniusz@hotmail.com>
2020-03-12 14:52:10 +0800

    working on product by sell and arrival

--- my-ecommerce-app-front/src/core/Home.js
+++ my-ecommerce-app-front/src/core/Home.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-const Home = () => (
-  <Layout title="Home Page" description="Node React e-Commerce App">
-    ...
-  </Layout>
-);
+import { getProducts, getProduct } from "./apiCore";
+
+const Home = () => {
+  const [productBySell, setProductBySell] = useState([]);
+  const [productByArrival, setProductByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProduct("sold").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProduct("createdAt").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
+  return (
+    <Layout title="Home Page" description="Node React e-Commerce App">
+      {JSON.stringify(productByArrival)}
+      <hr />
+      {JSON.stringify(productBySell)}
+    </Layout>
+  );
+};
 
 export default Home;
