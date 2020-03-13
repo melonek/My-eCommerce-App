1437419b477b93d183bd9535feed99306c5c6cf7
melon <meloniusz@hotmail.com>
2020-03-13 14:09:03 +0800

    progressing...

--- my-ecommerce-app-front/src/core/Shop.js
+++ my-ecommerce-app-front/src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
-import { getCategories } from "./apiCore";
+import { getCategories, getFilteredProducts } from "./apiCore";
 import Checkbox from "./Checkbox";
 import RadioBox from "./RadioBox";
 import { prices } from "./fixedPrices";
@@ -12,6 +12,9 @@ const Shop = () => {
   });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const init = () => {
     getCategories().then(data => {
@@ -19,6 +22,17 @@ const Shop = () => {
         setError(data.error);
       } else {
         setCategories(data);
+      }
+    });
+  };
+
+  const loadFilteredResults = newFilters => {
+    // console.log(newFilters);
+    getFilteredProducts(skip, limit, newFilters).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults(data);
       }
     });
   };
@@ -51,10 +65,6 @@ const Shop = () => {
     return array;
   };
 
-  const loadFilteredResults = newFilters => {
-    console.log(newFilters);
-  };
-
   return (
     <Layout
       title="Shop Plantex"
@@ -80,7 +90,7 @@ const Shop = () => {
           </div>
         </div>
 
-        <div className="col-8">{JSON.stringify(myFilters)}</div>
+        <div className="col-8">{JSON.stringify(filteredResults)}</div>
       </div>
     </Layout>
   );
