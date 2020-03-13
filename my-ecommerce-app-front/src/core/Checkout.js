915a28a161e70d6815259b4b60bb590c3b2f31a3
melon <meloniusz@hotmail.com>
2020-03-14 02:51:53 +0800

    completed the handling frontend part

--- my-ecommerce-app-front/src/core/Checkout.js
+++ my-ecommerce-app-front/src/core/Checkout.js
@@ -56,63 +56,23 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
   };
 
   const buy = () => {
-    setData({ loading: true });
-    // send the nonce to your server
-    // nonce = data.instance.requestPaymentMethod()
+    //send the nonce to your server
+    //nonce = data.instance.requestPaymentMethod()
     let nonce;
     let getNonce = data.instance
       .requestPaymentMethod()
       .then(data => {
-        // console.log(data);
-        nonce = data.nonce;
-        // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
-        // and also total to be charged
-        // console.log(
-        //     "send nonce and total to process: ",
-        //     nonce,
-        //     getTotal(products)
-        // );
-        const paymentData = {
-          paymentMethodNonce: nonce,
-          amount: getTotal(products)
-        };
-
-        processPayment(userId, token, paymentData)
-          .then(response => {
-            console.log(response);
-            // empty cart
-            // create order
-
-            const createOrderData = {
-              products: products,
-              transaction_id: response.transaction.id,
-              amount: response.transaction.amount,
-              address: deliveryAddress
-            };
-
-            createOrder(userId, token, createOrderData)
-              .then(response => {
-                emptyCart(() => {
-                  setRun(!run); // run useEffect in parent Cart
-                  console.log("payment success and empty cart");
-                  setData({
-                    loading: false,
-                    success: true
-                  });
-                });
-              })
-              .catch(error => {
-                console.log(error);
-                setData({ loading: false });
-              });
-          })
-          .catch(error => {
-            console.log(error);
-            setData({ loading: false });
-          });
+        console.log(data);
+        nonce = data.nonce; //once you have nonce (card type, card nuber) send nonce as "paymentMethodNonce"
+        //and also total to be charged
+        console.log(
+          "send nonce and total to process:",
+          nonce,
+          getTotal(products)
+        );
       })
       .catch(error => {
-        // console.log("dropin error: ", error);
+        console.log("dropin error: ", error);
         setData({ ...data, error: error.message });
       });
   };
@@ -148,9 +108,33 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
     </div>
   );
 
+  const showError = error => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = success => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      Thanks! Your payment was successful!
+    </div>
+  );
+
+  const showLoading = loading =>
+    loading && <h2 className="text-danger">Loading...</h2>;
+
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
+      {showLoading(data.loading)}
+      {showSuccess(data.success)}
+      {showError(data.error)}
       {showCheckout()}
     </div>
   );
