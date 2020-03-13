87b0d51aaa10685ff6baf6763473a18741d3d4ff
melon <meloniusz@hotmail.com>
2020-03-14 01:23:42 +0800

    getting the total - done

--- my-ecommerce-app-front/src/core/Checkout.js
+++ my-ecommerce-app-front/src/core/Checkout.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./apiCore";
 import Card from "./Card";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
 
 const Checkout = ({ products }) => {
   const getTotal = () => {
@@ -10,9 +12,20 @@ const Checkout = ({ products }) => {
     }, 0);
   };
 
+  const showCheckout = () => {
+    return isAuthenticated() ? (
+      <button className="btn btn-success">Checkout</button>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-primary">Sign in to checkout</button>
+      </Link>
+    );
+  };
+
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
+      {showCheckout()}
     </div>
   );
 };
