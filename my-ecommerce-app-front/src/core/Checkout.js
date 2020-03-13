78056fbe161017045b71a8a0dac29a40fbfcc9f6
melon <meloniusz@hotmail.com>
2020-03-14 02:10:17 +0800

    working on the client token div

--- my-ecommerce-app-front/src/core/Checkout.js
+++ my-ecommerce-app-front/src/core/Checkout.js
@@ -4,22 +4,45 @@ import { getProducts, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
-const Checkout = ({ products }) => {
-  const [data, setData] = {
+const Checkout = ({ products, setRun = f => f, run = undefined }) => {
+  const [data, setData] = useState({
     loading: false,
     success: false,
     clientToken: null,
     error: "",
     instance: {},
     address: ""
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+        setData({ ...data, error: data.error });
+      } else {
+        console.log(data);
+        setData({ clientToken: data.clientToken });
+      }
+    });
   };
 
-  
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
+  };
+
+  const handleAddress = event => {
+    setData({ ...data, address: event.target.value });
   };
 
   const showCheckout = () => {
@@ -31,6 +54,17 @@ const Checkout = ({ products }) => {
       </Link>
     );
   };
+
+const showDropIn = () => {
+    <div>
+        {data.clientToken !== null && products.length > 0 ? (
+            <div>
+                <DripIn options={{
+                    authorization: data.clientToken.clientToken
+                }}
+        ) : null}
+    </div>
+}
 
   return (
     <div>
