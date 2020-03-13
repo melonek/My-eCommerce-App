a8322b17fa98c273dc1009b1402fed2a9ecc7b9a
melon <meloniusz@hotmail.com>
2020-03-13 22:46:13 +0800

    css styling for single product and all product at home page

--- my-ecommerce-app-front/src/core/Card.js
+++ my-ecommerce-app-front/src/core/Card.js
@@ -34,12 +34,12 @@ const Card = ({ product, showViewProductButton = true }) => {
 
   return (
     <div className="card">
-      <div className="card-header card-header-1">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
         <p className="lead mt-2">{product.description.substring(0, 100)}</p>
-        <p className="black-9">${product.price}</p>
-        <p className="black-8">
+        <p className="black-10">${product.price}</p>
+        <p className="black-9">
           Category: {product.category && product.category.name}
         </p>
         <p className="black-8">
