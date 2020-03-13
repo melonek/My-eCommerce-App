542bb4155eab45f8574f4db94b0fbb7c50944b8a
melon <meloniusz@hotmail.com>
2020-03-14 02:34:10 +0800

    buy method

--- my-ecommerce-app-front/src/core/Checkout.js
+++ my-ecommerce-app-front/src/core/Checkout.js
@@ -55,16 +55,94 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
     );
   };
 
+  const buy = () => {
+    setData({ loading: true });
+    // send the nonce to your server
+    // nonce = data.instance.requestPaymentMethod()
+    let nonce;
+    let getNonce = data.instance
+      .requestPaymentMethod()
+      .then(data => {
+        // console.log(data);
+        nonce = data.nonce;
+        // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+        // and also total to be charged
+        // console.log(
+        //     "send nonce and total to process: ",
+        //     nonce,
+        //     getTotal(products)
+        // );
+        const paymentData = {
+          paymentMethodNonce: nonce,
+          amount: getTotal(products)
+        };
+
+        processPayment(userId, token, paymentData)
+          .then(response => {
+            console.log(response);
+            // empty cart
+            // create order
+
+            const createOrderData = {
+              products: products,
+              transaction_id: response.transaction.id,
+              amount: response.transaction.amount,
+              address: deliveryAddress
+            };
+
+            createOrder(userId, token, createOrderData)
+              .then(response => {
+                emptyCart(() => {
+                  setRun(!run); // run useEffect in parent Cart
+                  console.log("payment success and empty cart");
+                  setData({
+                    loading: false,
+                    success: true
+                  });
+                });
+              })
+              .catch(error => {
+                console.log(error);
+                setData({ loading: false });
+              });
+          })
+          .catch(error => {
+            console.log(error);
+            setData({ loading: false });
+          });
+      })
+      .catch(error => {
+        // console.log("dropin error: ", error);
+        setData({ ...data, error: error.message });
+      });
+  };
+
   const showDropIn = () => (
-    <div>
+    <div onBlur={() => setData({ ...data, error: "" })}>
       {data.clientToken !== null && products.length > 0 ? (
         <div>
+          <div className="gorm-group mb-3">
+            <label className="text-muted">Delivery address:</label>
+            <textarea
+              onChange={handleAddress}
+              className="form-control"
+              value={data.address}
+              placeholder="Type your delivery address here..."
+            />
+          </div>
+
           <DropIn
             options={{
-              authorization: data.clientToken.clientToken
+              authorization: data.clientToken,
+              paypal: {
+                flow: "vault"
+              }
             }}
             onInstance={instance => (data.instance = instance)}
           />
+          <button onClick={buy} className="btn btn-success btn-block">
+            Pay
+          </button>
         </div>
       ) : null}
     </div>
