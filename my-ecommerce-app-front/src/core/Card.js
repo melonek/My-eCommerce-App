86c84002da09aa362bcd9360c6e0ec4a2d86daaa
melon <meloniusz@hotmail.com>
2020-03-13 12:13:33 +0800

    working on the showImage section

--- my-ecommerce-app-front/src/core/Card.js
+++ my-ecommerce-app-front/src/core/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ShowImage from "./ShowImage";
 
 const Card = ({ product }) => {
   return (
     <div className="card">
       <div className="card-header">{product.name}</div>
       <div className="card-body">
+        <ShowImage item={product} url="product" />
         <p>{product.description}</p>
         <p>${product.price}</p>
         <Link to="/">
