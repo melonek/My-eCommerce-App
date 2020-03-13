327b0cd27d5f9bf3caccfc5108ed46a0f9032a42
melon <meloniusz@hotmail.com>
2020-03-13 20:58:51 +0800

    can retrieve data about single product from database succcessfully

--- my-ecommerce-app-front/src/core/Product.js
+++ my-ecommerce-app-front/src/core/Product.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts, getProduct } from "./apiCore";
+import { read } from "./apiCore";
 import Card from "./Card";
 
-const Product = () => {
+const Product = props => {
+  const [product, setProduct] = useState({});
+  const [error, setError] = useState(false);
+
+  const loadSingleProduct = productId => {
+    read(productId).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProduct(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const productId = props.match.params.productId;
+    loadSingleProduct(productId);
+  }, []);
+
   return (
     <Layout
       title="FullStack React Node MongoDB Ecommerce App"
       description="Node React E-commerce App"
       className="container-fluid"
     >
-      <p>product page</p>
+      <p className="mb-4">Single Product</p>
+      <div className="row">{JSON.stringify(product)}</div>
     </Layout>
   );
 };
