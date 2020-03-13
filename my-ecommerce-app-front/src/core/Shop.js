2e1bb3c12d0665b64ed36e098c47940e65daa390
melon <meloniusz@hotmail.com>
2020-03-13 14:34:20 +0800

    working on the load more button

--- my-ecommerce-app-front/src/core/Shop.js
+++ my-ecommerce-app-front/src/core/Shop.js
@@ -14,6 +14,7 @@ const Shop = () => {
   const [error, setError] = useState(false);
   const [limit, setLimit] = useState(6);
   const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
   const [filteredResults, setFilteredResults] = useState([]);
 
   const init = () => {
@@ -33,8 +34,34 @@ const Shop = () => {
         setError(data.error);
       } else {
         setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
     });
+  };
+
+  const loadMore = newFilters => {
+    let toSkip = skip + limit;
+    getFilteredProducts(toSkip, limit, newFilters.filters).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className="btn btn-warning mb-5">
+          Load more
+        </button>
+      )
+    );
   };
 
   useEffect(() => {
@@ -100,6 +127,7 @@ const Shop = () => {
               </div>
             ))}
           </div>
+          {loadMoreButton()}
         </div>
       </div>
     </Layout>
