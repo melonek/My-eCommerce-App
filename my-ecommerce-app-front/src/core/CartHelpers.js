4cae7e1eb90ae382657c7dc941491d07843dcfa5
melon <meloniusz@hotmail.com>
2020-03-16 15:32:39 +0800

    uploading git ignore to ignore env this time

--- my-ecommerce-app-front/src/core/CartHelpers.js
+++ my-ecommerce-app-front/src/core/CartHelpers.js
@@ -52,7 +52,7 @@ export const updateItem = (productId, count) => {
   }
 };
 
-export const removeItem = (productId, count) => {
+export const removeItem = productId => {
   let cart = [];
   if (typeof window !== "undefined") {
     if (localStorage.getItem("cart")) {
