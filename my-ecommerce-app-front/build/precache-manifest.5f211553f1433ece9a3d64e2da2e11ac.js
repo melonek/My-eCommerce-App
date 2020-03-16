536cebde4c15b640c7f395d0004da143bd0a7b19
melon <meloniusz@hotmail.com>
2020-03-16 21:21:28 +0800

    finilizing project

---
+++ my-ecommerce-app-front/build/precache-manifest.5f211553f1433ece9a3d64e2da2e11ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca01ecbf9f3c605ac181ee5c9908ec0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec1b353b93ed8c58d6ac",
+    "url": "/static/css/main.e753b6f8.chunk.css"
+  },
+  {
+    "revision": "e2ac36632e1ee4091b04",
+    "url": "/static/js/2.c19b77f5.chunk.js"
+  },
+  {
+    "revision": "ec1b353b93ed8c58d6ac",
+    "url": "/static/js/main.93d099e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
