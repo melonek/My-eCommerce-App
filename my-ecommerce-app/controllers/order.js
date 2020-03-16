4b7e88605f434f2f4b2209b3b5ffd830a7329d6f
melon <meloniusz@hotmail.com>
2020-03-16 17:05:07 +0800

    changes to the backend

--- my-ecommerce-app/controllers/order.js
+++ my-ecommerce-app/controllers/order.js
@@ -1,5 +1,22 @@
 const { Order, CartItem } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+// sendgrid for email npm i @sendgrid/mail
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey('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');
+
+exports.orderById = (req, res, next, id) => {
+  Order.findById(id)
+    .populate("products.product", "name price")
+    .exec((err, order) => {
+      if (err || !order) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      req.order = order;
+      next();
+    });
+};
 
 exports.create = (req, res) => {
   console.log("CREATE ORDER: ", req.body);
@@ -16,8 +33,8 @@ exports.create = (req, res) => {
     // order.products.length
     // order.amount
     const emailData = {
-      to: "kaloraat@gmail.com",
-      from: "noreply@ecommerce.com",
+      to: "",
+      from: "",
       subject: `A new order is received`,
       html: `
             <p>Customer name:</p>
@@ -26,6 +43,40 @@ exports.create = (req, res) => {
             <p>Login to dashboard to the order in detail.</p>
         `
     };
+    // sgMail.send(emailData);
     res.json(data);
   });
+};
+
+exports.listOrders = (req, res) => {
+  Order.find()
+    .populate("user", "_id name address")
+    .sort("-created")
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        });
+      }
+      res.json(orders);
+    });
+};
+
+exports.getStatusValues = (req, res) => {
+  res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateOrderStatus = (req, res) => {
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(order);
+    }
+  );
 };
--- my-ecommerce-app/routes/product.js
+++ my-ecommerce-app/routes/product.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  create,
-  productById,
-  read,
-  remove,
-  update,
-  list,
-  listRelated,
-  listCategories,
-  listBySearch,
-  photo,
-  listSearch
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo,
+    listSearch
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -20,18 +20,18 @@ const { userById } = require("../controllers/user");
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete(
-  "/product/:productId/:userId",
-  requireSignin,
-  isAuth,
-  isAdmin,
-  remove
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
 );
 router.put(
-  "/product/:productId/:userId",
-  requireSignin,
-  isAuth,
-  isAdmin,
-  update
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
 );
 
 router.get("/products", list);
